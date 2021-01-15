2b1778b49c73057d6bb1c04e2978dd68f72be348
Kevin Burk <percburk@gmail.com>
2021-01-15 15:55:37 -0600

    name change to gallery router

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const gallery = require('./routes/gallery.router.js');
+const gallery = require('./routes/gallery-router.js');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
